9adfb7a75e785e8193e33a0a27e0289cc10ca26c
cristina4321 <cristinadragusanu6@gmail.com>
2019-04-29 20:07:29 +0300

    Exercitiul 14

---
+++ src/ex14.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ex14 {
+
+    //14. scrieti o bucla infinita care va afisa de n ori "going".
+
+    public static void main(String[] args) {
+
+        int n=1;
+
+        while (n == 1 ) {
+            System.out.print(" going ");
+        }
+    }
+}
